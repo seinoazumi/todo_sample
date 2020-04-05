77659270dadf132b56280d06753926044248dec9
seinoazumi <seinoazumi@gmail.com>
2020-04-05 21:35:41 +0900

    second commit

---
+++ app/javascript/packs/components/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p>Qiita掲載の記事を参考に作成した、RailsでVue.jsを動かすプログラムです。</p>
+    <p>元の記事は<a href="https://qiita.com/naoki85/items/51a8b0f2cbf949d08b11" target="_blank">こちら</a>にあります。</p>
+  </div>
+</template>
